86d5bc73e020e4429d0c685d67241211a659b75e
Tanvir Islam <tanvir_cse07@yahoo.com>
2018-06-24 13:30:06 +0600

    Update installercentos.sh

--- installercentos.sh
+++ installercentos.sh
@@ -35,12 +35,13 @@ echo "Xinetd Package installed"
 echo "---------------------------------------"
 echo "Installing Nagios Remote Plugin Executor {NRPE} Version-3.2.1"
 cd /tmp
-#wget https://github.com/NagiosEnterprises/nrpe/releases/download/nrpe-3.2.1/nrpe-3.2.1.tar.gz
+wget https://github.com/NagiosEnterprises/nrpe/releases/download/nrpe-3.2.1/nrpe-3.2.1.tar.gz
 #wget https://github.com/amitanvir/nagios-plugin-and-nrpe-installer/raw/master/nrpe-2.12.tar.gz
-wget https://github.com/NagiosEnterprises/nrpe/archive/nrpe-2-12.tar.gz
+#wget https://github.com/NagiosEnterprises/nrpe/archive/nrpe-2-12.tar.gz
 tar -xzvf nrpe-*.tar.gz
 cd nrpe-nrpe-*
-./configure
+ln -s /usr/lib/x86_64-linux-gnu/libssl.so /usr/lib/libssl.so
+./configure --enable-ssl
 make all
 make install-plugin
 make install-daemon
@@ -50,5 +51,9 @@ read -p "Enter Nagios Monitor Server IP Address: " nagiosmonitorip
 mv /etc/xinetd.d/nrpe /etc/xinetd.d/nrpe.bak
 sed 's/127.0.0.1.*/127.0.0.1 '"$nagiosmonitorip"'/g' /etc/xinetd.d/nrpe.bak > /etc/xinetd.d/nrpe
 echo "nrpe            5666/tcp                        # NRPE" >> /etc/services
+cd /tmp
+wget http://dl.fedoraproject.org/pub/epel/6/i386/Packages/e/epel-release-6-8.noarch.rpm
+rpm -Uvh epel-release*rpm
+yum install perl-Sys-Statistics-Linux -y
 /etc/init.d/xinetd restart
 echo "I Hope It will be working"
